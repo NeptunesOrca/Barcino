b8bca77dea12fda3a5b9f277788aba134b73cc35
NeptunesOrca <83213467+NeptunesOrca@users.noreply.github.com>
2024-12-18 12:28:38 -0700

    Adjusted template for selection properties for better documentation

--- Scripts/Script Templates/Node/property_template.gd
+++ Scripts/Script Templates/Node/property_template.gd
@@ -2,6 +2,9 @@
 #meta-description: Template for creating a new SelectionProperty. Will automatically append "Property" to the end of whatever name is given
 extends SelectionProperty
 class_name _CLASS_Property
+## The [SelectionProperty] used to generate [_CLASS_PropertyField].
+##
+## Dictates the properties of a [_CLASS_PropertyField]. 
 
 #region Constants
 
