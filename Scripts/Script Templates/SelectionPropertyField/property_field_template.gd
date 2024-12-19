2ff78a24da4f9c0c33c22118ee251777cf302236
NeptunesOrca <83213467+NeptunesOrca@users.noreply.github.com>
2024-12-19 11:01:48 -0700

    Updated the property_field_template

--- Scripts/Script Templates/SelectionPropertyField/property_field_template.gd
+++ Scripts/Script Templates/SelectionPropertyField/property_field_template.gd
@@ -1,7 +1,7 @@
-#meta-name: New PropertyField
+#meta-name: New SelectionPropertyField
 #meta-description: Template for creating a new PropertyFields. Will automatically append "PropertyField" to the end of whatever name is given
 #meta-default: true
-extends PropertyField
+extends SelectionPropertyField
 class_name _CLASS_PropertyField
 
 #region Member Variables
@@ -10,9 +10,9 @@ class_name _CLASS_PropertyField
 
 #region Initialization
 ## Class Initialization
-func _init(obj : DraggableObject, property : _CLASS_Property).(obj,property):
-	#Note that PropertyField will run its initialization before this subclass
-	pass
+func _init(obj : DraggableObject, property : _CLASS_Property):
+	super(obj, property) #calls SelectionPropertyField._init()
+
 #end region
 
 #region Functions
